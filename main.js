d1f691aaf3e63f9d10196a52c7a0eac20dedda51
acheatham87 <acheatham87@gmail.com>
2022-02-17 13:49:36 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// const fillGasTank = () => {
+//     const gallons = 15
+// }
+
+// fillGasTank()
+
+
+// const fillGasTank = (gallons) => {
+//         if (gallons > 15) {
+//         console.log("You cannot add this much gas to the tank.")
+//     }
+//     else {
+//         console.log(`I filled the tank with ${gallons} of gas.`)
+//     }
+// }
+
+// fillGasTank(15)
+// fillGasTank(22)
+
+const findHalf = (scaryNumber) => {
+    const half = scaryNumber / 2
+    return half
+}
+
+const returnedNumber = findHalf(42)
+
+console.log(returnedNumber)
+
